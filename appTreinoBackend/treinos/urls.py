4bbc199d636cdd341f601d985ebfbcd27f5d38cd
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-09-11 22:26:31 +0900

    Feat: inclusão das views de exercicios

--- appTreinoBackend/treinos/urls.py
+++ appTreinoBackend/treinos/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from .views import ListarExercicios, EditarExercicios
 from .views.view_treinos import ListarTreinos, EditarTreinos
 
 
 urlpatterns = [
     path('treinos/', ListarTreinos.as_view(), name='treinos-list-create'),
     path('treinos/<uuid:pk>/', EditarTreinos.as_view(), name='treino-edit'),
+    path('exercicios/', ListarExercicios.as_view(), name='exercicios-list-create'),
+    path('exercicios/<uuid:pk>', EditarExercicios.as_view(), name='exercicios-edit'),
 ]
