985a1247921ea74be2202386726c91b42c7c62dd
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-10-10 21:57:30 +0900

    Fix: Adicionando a barra ao final da rota para seguir o padrão

--- appTreinoBackend/treinos/urls.py
+++ appTreinoBackend/treinos/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('treinos/', ListarTreinos.as_view(), name='treinos-list-create'),
     path('treinos/<uuid:pk>/', EditarTreinos.as_view(), name='treino-edit'),
     path('exercicios/', ListarExercicios.as_view(), name='exercicios-list-create'),
-    path('exercicios/<uuid:pk>', EditarExercicios.as_view(), name='exercicios-edit'),
+    path('exercicios/<uuid:pk/>', EditarExercicios.as_view(), name='exercicios-edit'),
 ]
