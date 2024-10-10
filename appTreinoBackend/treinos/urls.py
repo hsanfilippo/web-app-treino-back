e8fd44e65322edc87738f4d3a63517bd078f34ca
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-10-10 22:51:10 +0900

    Fix: Correção do lookup passado nas rotas de pk para id

--- appTreinoBackend/treinos/urls.py
+++ appTreinoBackend/treinos/urls.py
@@ -6,7 +6,7 @@ from .views.view_treinos import ListarTreinos, EditarTreinos
 
 urlpatterns = [
     path('treinos/', ListarTreinos.as_view(), name='treinos-list-create'),
-    path('treinos/<uuid:pk>/', EditarTreinos.as_view(), name='treino-edit'),
+    path('treinos/<uuid:id>/', EditarTreinos.as_view(), name='treino-edit'),
     path('exercicios/', ListarExercicios.as_view(), name='exercicios-list-create'),
-    path('exercicios/<uuid:pk>/', EditarExercicios.as_view(), name='exercicios-edit'),
+    path('exercicios/<uuid:id>/', EditarExercicios.as_view(), name='exercicios-edit'),
 ]
