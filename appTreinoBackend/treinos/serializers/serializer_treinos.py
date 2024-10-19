c042d7edcfe643cbb6a6948c0b96b47e8a9d9bdd
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-10-19 23:39:37 +0900

    Feat: Configuração para a API aceitar um exercício ao criar um treino

--- appTreinoBackend/treinos/serializers/serializer_treinos.py
+++ appTreinoBackend/treinos/serializers/serializer_treinos.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 
+from ..models import Exercicios
 from ..serializers.serializer_exercicios import SerializerExercicios
 from ..models.treinos import Treinos
 
 
 class SerializerTreinos(serializers.ModelSerializer):
-    exercicios = SerializerExercicios(many=True, read_only=True)
+    exercicios = SerializerExercicios(many=True)
 
     class Meta:
         model = Treinos
         fields = '__all__'
+
+    def create(self, validated_data):
+        exercicios_data = validated_data.pop('exercicios')
+
+        treino = Treinos.objects.create(**validated_data)
+
+        for exercicio_data in exercicios_data:
+            Exercicios.objects.create(treino=treino, **exercicio_data)
+
+        return treino
