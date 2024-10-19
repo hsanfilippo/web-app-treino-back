b15f6de9175eb6f693b7b37fb775b8c7d84546d5
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-10-19 23:38:15 +0900

    Update: Alterado para aceitar valores nulos na criação de um exercicio

--- appTreinoBackend/treinos/models/exercicios.py
+++ appTreinoBackend/treinos/models/exercicios.py
@@ -7,13 +7,13 @@ from ..models.treinos import Treinos
 
 class Exercicios(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    agrup_musc = models.TextField()
+    agrup_musc = models.TextField(null=True, default='')
     nome_exerc = models.CharField(max_length=30)
     series = models.IntegerField()
     reps = models.IntegerField()
     carga = models.IntegerField()
     interv_seg = models.IntegerField()
-    tecnica_avanc = models.TextField(null=True)
+    tecnica_avanc = models.TextField(null=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     treino = models.ForeignKey(Treinos, related_name='exercicios', null=True, on_delete=models.CASCADE)
