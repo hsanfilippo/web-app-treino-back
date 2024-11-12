2b7e806e01cd2ed47a9ee91ccab4b4efaa98734e
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-11-13 07:42:29 +0900

    Fix: Especificando para o serializer que os valores de agrup_musc e tecnica_avanc podem ser passados em branco no payload da requisição.

--- appTreinoBackend/treinos/serializers/serializer_exercicios.py
+++ appTreinoBackend/treinos/serializers/serializer_exercicios.py
@@ -3,6 +3,9 @@ from ..models.exercicios import Exercicios
 
 
 class SerializerExercicios(serializers.ModelSerializer):
+    agrup_musc = serializers.CharField(allow_blank=True, required=False)
+    tecnica_avanc = serializers.CharField(allow_blank=True, required=False)
+
     class Meta:
         model = Exercicios
         fields = '__all__'
