0adab92dfa086cf16eb9c560146e97bac19845db
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-11-13 07:43:31 +0900

    Feat: Criação do método update (PUT) para os exercícios de um treino.

--- appTreinoBackend/treinos/serializers/serializer_treinos.py
+++ appTreinoBackend/treinos/serializers/serializer_treinos.py
@@ -21,3 +21,27 @@ class SerializerTreinos(serializers.ModelSerializer):
             Exercicios.objects.create(treino=treino, **exercicio_data)
 
         return treino
+
+    def update(self, instance, validated_data):
+        exercicios_data = validated_data.pop('exercicios', None)
+
+        instance.nome = validated_data.get('nome', instance.nome)
+        instance.descricao = validated_data.get('descricao', instance.descricao)
+        instance.save()
+
+        if exercicios_data:
+            ids_exercicios = [ex_data.get('id') for ex_data in exercicios_data if ex_data.get('id')]
+
+            Exercicios.objects.filter(treino=instance).exclude(id__in=ids_exercicios).delete()
+
+            for exercicio_data in exercicios_data:
+                exercicio_id = exercicio_data.get('id')
+                if exercicio_id:
+                    exercicio = Exercicios.objects.get(id=exercicio_id, treino=instance)
+                    for attr, value in exercicio_data.items():
+                        setattr(exercicio, attr, value)
+                    exercicio.save()
+                else:
+                    Exercicios.objects.create(**exercicio_data)
+
+        return instance
